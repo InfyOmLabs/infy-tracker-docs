8e98bd80496ab8405785b11ab4084999416e6c80
Harshad <harshad.prajapati@infyom.com>
2019-08-12 09:58:52 +0530

    feat(home-page): fixes of link path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,12 @@ module.exports = {
     title: 'InfyTracker',
     description: 'Minimal Task and Time Management tool',
     head: [
-        ['link', { rel: 'icon', href: 'assets/img/favicon.ico' }],
-        ["script", { src: "assets/js/jquery-3.3.1.min.js" }],
-        ["script", { src: "assets/js/popper.min.js" }],
-        ["script", { src: "assets/js/bootstrap.min.js" }],
-        ["script", { src: "assets/js/contact.js" }],
-        ["script", { src: "assets/js/validate.js" }],
+        ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
+        ["script", { src: "/assets/js/jquery-3.4.1.min.js" }],
+        ["script", { src: "/assets/js/popper.min.js" }],
+        ["script", { src: "/assets/js/bootstrap.min.js" }],
+        ["script", { src: "/assets/js/contact.js" }],
+        ["script", { src: "/assets/js/validate.js" }],
     ],
     themeConfig: {
         search: false,
